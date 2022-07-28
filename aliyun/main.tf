9c4ba6be36624ebb4bf6fd7ffcf3625719f3427c
YangKian <1207783292@qq.com>
2022-07-28 14:18:40 +0800

    chore: add aliyun service

---
+++ aliyun/main.tf
@@ -0,0 +1,146 @@
+terraform {
+  required_version = ">=1.121.0"
+  required_providers {
+    alicloud = {
+      source  = "hashicorp/alicloud"
+      version = "1.178.0"
+    }
+  }
+}
+
+provider "alicloud" {
+  region = var.region
+  # setting `access_key` and `secret_key` via environment
+  # variables `ALICLOUD_ACCESS_KEY` and `ALICLOUD_SECRET_KEY`
+}
+
+module "ali_vpc" {
+  source         = "./modules/vpc"
+  vpc_cidr_block = var.vpc_cidr_block
+  vpc_name       = "hstream"
+  vpc_zone       = var.zone
+  vsw_cidr_block = var.vsw_cidr_block
+}
+
+module "ali_sec_group" {
+  source                   = "./modules/security_group"
+  vpc_id                   = module.ali_vpc.vpc_id
+  ingress_with_cidr_blocks = var.ingress_with_cidr_blocks
+  egress_with_cidr_blocks  = var.egress_with_cidr_blocks
+}
+
+resource "alicloud_instance" "storage_instance" {
+  count             = var.storage_instance_config.node_count
+  availability_zone = var.zone
+  security_groups   = [module.ali_sec_group.sec_group_id]
+
+  instance_type              = var.storage_instance_config.instance_type
+  key_name                   = ""
+  system_disk_category       = var.storage_instance_config.system_disk_category
+  system_disk_size           = var.storage_instance_config.system_disk_size
+  image_id                   = var.image_id
+  instance_name              = "hserver-${count.index}"
+  vswitch_id                 = module.ali_vpc.vsw_id
+  internet_max_bandwidth_out = var.internet_max_bandwidth_out
+  internet_charge_type       = var.internet_charge_type
+  user_data                  = <<-EOF
+    #!/bin/bash
+    echo "==== mount disks ===="
+    sudo mkdir /data
+    sudo mkdir /mnt/data{0,1}
+    sudo mkfs -t ext4 /dev/nvme1n1
+    sudo mkfs -t ext4 /dev/nvme2n1
+    sudo mount /dev/nvme1n1 /mnt/data0
+    sudo mount /dev/nvme2n1 /mnt/data1
+    sudo chown -R ubuntu /data
+    sudo chown -R ubuntu /mnt
+  EOF
+}
+
+resource "alicloud_instance" "calculate_instance" {
+  count             = var.calculate_instance_config.node_count
+  availability_zone = var.zone
+  security_groups   = [module.ali_sec_group.sec_group_id]
+
+  instance_type              = var.calculate_instance_config.instance_type
+  key_name                   = ""
+  system_disk_category       = var.calculate_instance_config.system_disk_category
+  system_disk_size           = var.calculate_instance_config.system_disk_size
+  image_id                   = var.image_id
+  instance_name              = "hserver-${count.index}"
+  vswitch_id                 = module.ali_vpc.vsw_id
+  internet_max_bandwidth_out = var.internet_max_bandwidth_out
+  internet_charge_type       = var.internet_charge_type
+}
+
+resource "alicloud_key_pair_attachment" "attachment" {
+  key_pair_name = var.key_pair_name
+  instance_ids  = alicloud_instance.storage_instance.*.id
+}
+
+# ------------ step ------------
+
+# ----------- generate config file -------------------
+resource "local_file" "config" {
+  depends_on = [alicloud_instance.storage_instance, alicloud_instance.calculate_instance]
+  filename   = "../file/config.json"
+  content = templatefile("../file/clusterCfg.json.tftpl", {
+    server_hosts = jsonencode(
+      merge(
+        { for idx, s in alicloud_instance.storage_instance.* : "hs-s${idx + 1}" => [s.public_ip, s.private_ip] },
+        { for idx, s in alicloud_instance.calculate_instance.* : "hs-c${idx + 1}" => [s.public_ip, s.private_ip] }
+  )) })
+}
+
+resource "null_resource" "echo_config" {
+  depends_on = [local_file.config]
+
+  provisioner "local-exec" {
+    command = "cat ${local_file.config.filename}"
+  }
+}
+
+# ---------- start server & client ---------------------
+
+resource "null_resource" "start-server" {
+  depends_on = [local_file.config]
+  count      = var.storage_instance_config.node_count
+
+  connection {
+    user        = "ubuntu"
+    private_key = file(var.private_key_path)
+    host        = alicloud_instance.storage_instance[count.index].public_ip
+  }
+
+  provisioner "remote-exec" {
+    script = "../file/server-node-start.sh"
+  }
+}
+
+resource "null_resource" "start-client" {
+  depends_on = [local_file.config]
+  count      = var.calculate_instance_config.node_count
+
+  connection {
+    user        = "ubuntu"
+    private_key = file(var.private_key_path)
+    host        = alicloud_instance.calculate_instance[count.index].public_ip
+  }
+
+  provisioner "remote-exec" {
+    script = "../file/client-node-start.sh"
+  }
+}
+
+# -------------- dump net topology -----------------------
+
+resource "null_resource" "dump_topology" {
+  depends_on = [
+    alicloud_instance.storage_instance,
+    alicloud_instance.calculate_instance
+  ]
+
+  provisioner "local-exec" {
+    command = "terraform output -json > ../file/topology"
+  }
+}
---
+++ aliyun/variables.tf
@@ -0,0 +1,132 @@
+## Common
+
+variable "region" {
+  type        = string
+  description = "region name"
+  default     = "cn-zhangjiakou"
+  sensitive   = true
+}
+
+variable "zone" {
+  type        = string
+  description = "zone name"
+  default     = "cn-zhangjiakou-a"
+  sensitive   = true
+}
+
+variable "image_id" {
+  type        = string
+  default     = "ubuntu_20_04_x64_20G_alibase_20210623.vhd"
+  description = "image id"
+}
+
+variable "key_pair_name" {
+  type      = string
+  sensitive = true
+}
+
+variable "private_key_path" {
+  type      = string
+  sensitive = true
+}
+
+## Vpc
+
+variable "ecs_vswitch_conf" {
+  type = object({
+    name   = string
+    region = string
+    cidr   = string
+  })
+  description = "vswitch configurations"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  default     = ""
+  description = "cidr of vpc"
+}
+
+variable "vsw_cidr_block" {
+  type        = string
+  default     = ""
+  description = "cidr of vsw"
+}
+
+## Security Group
+
+variable "security_group_name" {
+  type        = string
+  default     = "hstream"
+  description = "security group name"
+}
+
+variable "ingress_with_cidr_blocks" {
+  type        = list(any)
+  default     = [null]
+  description = "ingress with cidr blocks"
+}
+
+variable "egress_with_cidr_blocks" {
+  type        = list(any)
+  default     = [null]
+  description = "egress with cidr blocks"
+}
+
+## Ecs
+
+variable "storage_instance_config" {
+  type = object({
+    node_count           = number
+    instance_type        = string
+    system_disk_category = string
+    system_disk_size     = number
+  })
+}
+
+variable "calculate_instance_config" {
+  type = object({
+    node_count           = number
+    instance_type        = string
+    system_disk_category = string
+    system_disk_size     = number
+  })
+}
+
+variable "storage_instance_type" {
+  type        = string
+  default     = "ecs.i2gne.2xlarge"
+  description = "hstream-server instance type"
+}
+
+variable "calculate_instance_type" {
+  type        = string
+  default     = "ecs.i2gne.2xlarge"
+  description = "hstream-server instance type"
+}
+
+variable "internet_max_bandwidth_out" {
+  type        = number
+  default     = 10
+  description = "internet max bandwidth out"
+}
+
+variable "internet_charge_type" {
+  type        = string
+  default     = "PayByTraffic"
+  description = "charge type"
+}
+
+## clb
+
+variable "listener_tcp_ports" {
+  type        = list(number)
+  default     = []
+  description = "the tcp listener ports of clb"
+}
+
+variable "listener_http_ports" {
+  type        = list(number)
+  default     = []
+  description = "the http listener ports of clb"
+}
